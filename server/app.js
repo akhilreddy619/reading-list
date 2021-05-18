ee5153af07af2452141067ffc60e77608c647ee9
Akhil Reddy Duggasani <akhilredy.duggasani@gmail.com>
2021-05-18 14:04:56 +0530

    Complete Code

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect(
+  "mongodb+srv://akhil:test123@gql-ninja.crb1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
+
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
